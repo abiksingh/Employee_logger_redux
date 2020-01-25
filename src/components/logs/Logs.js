94dd9dfa89860ca3546b31ccdf6ba85ec9e6254a
ABHISHEK SINGH <50770382+abiksingh@users.noreply.github.com>
2020-01-25 02:40:44 +0100

    initial commit

---
+++ src/components/logs/Logs.js
@@ -0,0 +1,44 @@
+import React,{useEffect} from 'react';
+import {connect} from 'react-redux';
+import LogItem from '../logs/LogItem';
+import Preloader from '../layouts/Preloader';
+import PropTypes from 'prop-types';
+import {getLogs} from '../../actions/logActions';
+
+
+const Logs = ({log:{logs,loading}, getLogs}) => {
+
+    // const [logs, setLogs] = useState([]);
+    // const [loading, setLoading] = useState(false);
+
+
+    useEffect(()=>{
+        getLogs();
+      // eslint-disable-next-line  
+    },[])
+
+    
+    if(loading || logs === null){
+        return <Preloader />
+    }
+
+    return (
+        <ul className="collection with-header">
+            <li className="collection-header">
+                <h4 className="center">System Logs</h4>
+            </li>
+            {!loading && logs.length === 0 ? (<p className="center">No logs to Show...</p>) : (logs.map(log => <LogItem log={log} key={log.id} />)) }
+        </ul>
+    )
+}
+
+Logs.propTypes = {
+    log: PropTypes.object.isRequired,
+    getLogs: PropTypes.func.isRequired
+}
+
+const mapStateToProps =(state)=> ({
+    log: state.log
+})
+
+export default connect(mapStateToProps,{getLogs})(Logs)
